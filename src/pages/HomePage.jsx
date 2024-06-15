520fb605df55c4699454118b23653156a19fc991
Micheal Owen <mowen8983@gmail.com>
2024-06-15 20:12:28 +0700

    feat: add Carousels and title

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
 import NavBar from "../components/organisms/NavBar";
-
+import Carousels from "../components/organisms/Carousels";
 const HomePage = () => {
   return (
     <>
+      <div className="text-center py-6">
+        <h1 className="text-[26px]  text-[#BEA20B] font-oleoScripts">
+          Goods Hive
+        </h1>
+      </div>
+      <div>
+        <Carousels />
+      </div>
       <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 flex gap-8">
         <NavBar />
       </div>
