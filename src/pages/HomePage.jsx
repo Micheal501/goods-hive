d505c3f3ac35bd2a177c8b783c910b5aabd94b9d
Micheal Owen <mowen8983@gmail.com>
2024-06-15 19:03:10 +0700

    feat: add HomePage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/organisms/NavBar";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 flex gap-8">
+        <NavBar />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
