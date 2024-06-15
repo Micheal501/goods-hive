4a4895bd167f20abd82b4f833de7e13f8e4fa124
Micheal Owen <mowen8983@gmail.com>
2024-06-15 18:57:25 +0700

    feat: add roboto font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
