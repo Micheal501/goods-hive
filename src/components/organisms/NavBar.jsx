b77144d4a6f5853e7867efbd477d53515c486222
Micheal Owen <mowen8983@gmail.com>
2024-06-15 19:02:41 +0700

    feat: add NavBar icons

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -1,20 +1,46 @@
 import NavLinkList from "../molecules/NavLinkList";
+import { Home } from "lucide-react";
+import { AlignLeft } from "lucide-react";
+import { CircleUser } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+
 const NavBar = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col justify-center items-center">
+        <a href="/">
+          <Home className="w-12 h-12" />
+        </a>
         <NavLinkList
           NavLinkTitle="Home"
           NavLinkUrl="/"
         />
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <a href="/category">
+          <AlignLeft className="w-12 h-12" />
+        </a>
         <NavLinkList
           NavLinkTitle="Category"
           NavLinkUrl="/category"
         />
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <a href="/login">
+          <CircleUser className="w-12 h-12" />
+        </a>
         <NavLinkList
           NavLinkTitle="Login"
           NavLinkUrl="/login"
         />
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <a href="/cart">
+          <ShoppingCart className="w-12 h-12" />
+        </a>
         <NavLinkList
           NavLinkTitle="Cart"
           NavLinkUrl="/cart"
